2973e921481b01ca7185e28fbdb5f1a021257d4a
Luis Fellipe Franco Arakaki <luisfellipe.arakaki@gmail.com>
2026-04-13 17:10:43 -0300

    feat(p1): implementa captura de dados

--- investimentos/src/App.jsx
+++ investimentos/src/App.jsx
@@ -1,17 +1,56 @@
+import CapturaDados from './CapturaDados'
+import { useState } from 'react'
+
 const App = () => {
+
+  const [valorInicial, setValorInicial] = useState(null)
+  const [aporteMensal, setAporteMensal] = useState(null)
+  const [taxaJuros, setTaxaJuros] = useState(null)
+  const [periodoMeses, setPeriodo] = useState(null)
+
+  const calcularValor = () => {
+
+    const valor = Number(valorInicial)
+    const aporte = Number(aporteMensal)
+    const juros = Number(taxaJuros)
+    const periodo = Number(periodoMeses)
+
+    if(valor && aporte && juros && periodo)
+    {
+      if(valor < 0 || aporte < 0 || juros < 0 || periodo < 0)
+      {
+        alert("Todos os valores devem ser positivos!")
+      }
+      else
+      {
+
+      }
+    }
+    else
+    {
+      alert("Todos os campos precisam estar preenchidos com valores numéricos! Para números decimais, utilize ponto e não vírgula")
+    }
+  }
+
+
   return ( 
-    <div className="container border mt-2">
-
-      <div className="row">
-
+    <div className="container mt-2">
+      <div className="row justify-content-center">
         <div className="col-12 text-center">
           <h1>
             Hello, Investimentos
           </h1>
         </div>
+
+        <CapturaDados
+          setValorInicial={setValorInicial}
+          setAporteMensal={setAporteMensal}
+          setTaxaJuros={setTaxaJuros}
+          setPeriodo={setPeriodo}
+          calcularValor={calcularValor}
+        />
         
       </div>
-
     </div>
   )
 }
---
+++ investimentos/src/CapturaDados.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+const CapturaDados = (props) => {
+
+    return (
+        <div className='container'>
+
+            <div className="row justify-content-center">
+
+                <div className="col-md-6 col-sm-12">
+                    <p>Valor inicial (R$)</p>
+                    <input 
+                        type="text"
+                        placeholder="Ex: 1000.00" 
+                        style={{width: "100%", height: 50, marginBottom: 10, borderRadius: 10, paddingBottom: 3, paddingLeft: 7}}
+                        onChange={(e) => {props.setValorInicial(e.target.value)}}
+                    />    
+                </div>
+
+                <div className="col-md-6 col-sm-12">
+                    <p>Aporte mensal (R$)</p>
+                    <input 
+                        type="text"
+                        placeholder="Ex: 300.00" 
+                        style={{width: "100%", height: 50, marginBottom: 10, borderRadius: 10, paddingBottom: 3, paddingLeft: 7}}
+                        onChange={(e) => {props.setAporteMensal(e.target.value)}}
+                    />
+                </div>
+
+                <div className="col-md-6 col-sm-12">
+                    <p>Taxa de juros (% ao mês)</p>
+                    <input 
+                        type="text"
+                        placeholder="Ex: 0.85" 
+                        style={{width: "100%", height: 50, marginBottom: 10, borderRadius: 10, paddingBottom: 3, paddingLeft: 7}}
+                        onChange={(e) => {props.setTaxaJuros(e.target.value)}}
+                    />
+                </div>
+
+                <div className="col-md-6 col-sm-12">
+                    <p>Período (meses)</p>
+                    <input 
+                        type="text"
+                        placeholder="Ex: 24" 
+                        style={{width: "100%", height: 50, marginBottom: 10, borderRadius: 10, paddingBottom: 3, paddingLeft: 7}}
+                        onChange={(e) => {props.setPeriodo(e.target.value)}}
+                    />
+                </div>
+
+            </div>
+
+            <div className="row mt-2">
+
+                <div className="col-lg-9 col-md-6 col-sm-12 mb-2">
+                    <button 
+                        className="btn btn-primary w-100 h-100 mt-3"
+                        onClick={props.calcularValor}
+                    >
+                        Calcular
+                    </button>
+                </div>
+
+                <div className="col-lg-3 col-md-6 col-sm-12 mb-2">
+                    <button 
+                        className="btn btn-danger w-100 h-100 mt-3">
+                        Limpar
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default CapturaDados
