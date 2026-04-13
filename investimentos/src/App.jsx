0d615e771e8121160b74491b4ff6f5dd0e3a0e46
Luis Fellipe Franco Arakaki <luisfellipe.arakaki@gmail.com>
2026-04-13 19:03:55 -0300

    feat(p1): implementa calculo e exibiçao de resultados

--- investimentos/src/App.jsx
+++ investimentos/src/App.jsx
@@ -1,3 +1,4 @@
+import ExibeDados from './ExibeDados'
 import CapturaDados from './CapturaDados'
 import { useState } from 'react'
 
@@ -6,24 +7,35 @@ const App = () => {
   const [valorInicial, setValorInicial] = useState(null)
   const [aporteMensal, setAporteMensal] = useState(null)
   const [taxaJuros, setTaxaJuros] = useState(null)
-  const [periodoMeses, setPeriodo] = useState(null)
+  const [periodoMeses, setPeriodo] = useState(0)
+  const [valorFinal, setValorFinal] = useState(0)
+  const [totalInvestido, setTotalInvestido] = useState(0)
+  const [totalJuros, setTotalJuros] = useState(0)
+  const [rentabilidade, setRentabilidade] = useState(0)
 
   const calcularValor = () => {
 
     const valor = Number(valorInicial)
     const aporte = Number(aporteMensal)
-    const juros = Number(taxaJuros)
+    const taxa = Number(taxaJuros)/100
     const periodo = Number(periodoMeses)
 
-    if(valor && aporte && juros && periodo)
+    if(valor && aporte && taxa && periodo)
     {
-      if(valor < 0 || aporte < 0 || juros < 0 || periodo < 0)
+      if(valor < 0 || aporte < 0 || taxa < 0 || periodo < 0)
       {
         alert("Todos os valores devem ser positivos!")
       }
       else
       {
-
+        const final = (valor*((1+taxa)**periodo))+(aporte*((((1+taxa)**periodo)-1)/taxa))
+        setValorFinal(final)
+        const investido = valor + (aporte*periodo)
+        setTotalInvestido(investido)
+        const juros = final - investido
+        setTotalJuros(juros)
+        const lucro = (juros/investido)*100
+        setRentabilidade(lucro)
       }
     }
     else
@@ -49,7 +61,13 @@ const App = () => {
           setPeriodo={setPeriodo}
           calcularValor={calcularValor}
         />
-        
+        <ExibeDados
+          valorFinal={valorFinal}
+          totalInvestido={totalInvestido}
+          totalJuros={totalJuros}
+          numeroAportes={periodoMeses}
+          rentabilidade={rentabilidade}
+        />
       </div>
     </div>
   )
---
+++ investimentos/src/ExibeDados.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const ExibeDados = (props) => {
+    return (
+        <div className="container bg-light rounded mt-5">
+            <div className="row mt-3">
+                <div className="col-12">
+                    <p>Valor final acumulado</p>
+                    <h1 class="text-success">R$ {props.valorFinal}</h1>
+                </div>
+            </div>
+            <div className="row mt-5">
+                <div className="col-md-6 col-sm-12">
+                    <p>Total investido</p>
+                    <h3>R$ {props.totalInvestido}</h3>
+                </div>
+                <div className="col-md-6 col-sm-12">
+                    <p>Juros acumulados</p>
+                    <h3>R$ {props.totalJuros}</h3>
+                </div>
+                <div className="col-md-6 col-sm-12">
+                    <p>N de aportes</p>
+                    <h3>{props.numeroAportes}</h3>
+                </div>
+                <div className="col-md-6 col-sm-12">
+                    <p>Rentabilidade</p>
+                    <h3>+{props.rentabilidade}%</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ExibeDados
